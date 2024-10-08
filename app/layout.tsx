3c107d1c9ddfb4d6dbee638116ecda9f854973e7
lasbe <devlasbe@gmail.com>
2024-10-09 04:55:08 +0900

    chore: 메타데이터 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,17 @@
 import type { Metadata } from "next";
 import { Noto_Sans_KR } from "next/font/google";
 import "./globals.css";
+import Footer from "@/components/footer";
+
+/**
+ * @todo
+ * 1. 메타데이타
+ * 2. robot
+ * 3. search console
+ * 4. footer
+ * 5. refactoring
+ * 6. loading
+ */
 
 const notoSansKr = Noto_Sans_KR({
   subsets: ["latin"],
@@ -11,6 +22,7 @@ const notoSansKr = Noto_Sans_KR({
 export const metadata: Metadata = {
   title: "안성재 GPT",
   description: "안성재의 요리평가 GPT",
+  keywords: "흑백요리사, 넷플릭스, 안성재, 최현석, chat gpt, 안성재 gpt",
 };
 
 export default function RootLayout({
@@ -20,7 +32,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${notoSansKr.variable}  antialiased`}>{children}</body>
+      <body className={`${notoSansKr.variable} antialiased`}>
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
