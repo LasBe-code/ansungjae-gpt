cbe09048156153e6bf7f7f4c55e792a254d7d1f3
lasbe <devlasbe@gmail.com>
2024-10-10 22:06:38 +0900

    refactor: button 분리

---
+++ components/ui/button.tsx
@@ -0,0 +1,17 @@
+import { ButtonHTMLAttributes } from "react";
+
+type ButtonType = {
+  children: React.ReactNode;
+} & ButtonHTMLAttributes<HTMLButtonElement>;
+
+export default function Button({ children, ...props }: ButtonType) {
+  const { className, ...rest } = props;
+  return (
+    <button
+      {...rest}
+      className={`py-2 px-4 rounded-md bg-neutral-700 hover:bg-neutral-900 disabled:bg-neutral-700 disabled:text-transparent font-bold text-sm text-neutral-100 ${className}`}
+    >
+      {children}
+    </button>
+  );
+}
